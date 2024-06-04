5d4f795f2d287e11de09022ca5f3e9d7f4f0507d
ZhechoZhechev <mishomishev024@gmail.com>
2024-06-04 07:55:15 +0300

    Loading the api key implemented

--- Program.cs
+++ Program.cs
@@ -1,6 +1,27 @@
 ﻿namespace Currency_conversion_CLI;
 
+using Newtonsoft.Json;
+using System.IO;
+
 public class Program
 {
+    private static string apiKey = null!;
 
+
+    private static void LoadApiKey()
+    {
+        try
+        {
+            var file = File.ReadAllText("config.json");
+            var config = JsonConvert.DeserializeObject<Dictionary<string, string>>(file);
+            apiKey = config!["api_key"];
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Unable to load the API key.");
+        }
+
+    }
 }
+
+
