2f4372f00aaa74364546eefe83906b4637767eed
ZhechoZhechev <mishomishev024@gmail.com>
2024-06-04 08:44:02 +0300

    validating the date method

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace Currency_conversion_CLI;
 
 using Newtonsoft.Json;
+using System.Globalization;
 using System.IO;
 
 public class Program
@@ -10,6 +11,16 @@ public class Program
     public static void Main(string[] args)
     {
         LoadApiKey();
+
+        Console.WriteLine("Welcome to Currency Conversion CLI!");
+
+        string dateInput = args.Length > 0 ? args[0] : string.Empty;
+
+        while (!ValidateDateIput(dateInput))
+        {
+            Console.WriteLine("Please enter conversion rate date in the format YYYY-MM-DD:");
+            dateInput = Console.ReadLine()!;
+        }
     }
     private static void LoadApiKey()
     {
@@ -24,6 +35,11 @@ public class Program
             Console.WriteLine("Unable to load the API key.");
         }
 
+    }
+
+    private static bool ValidateDateIput(string dateInput)
+    {
+        return DateTime.TryParseExact(dateInput, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out _);
     }
 }
 
