ce0f557d54547694cb5289130ae272833e548ec4
ZhechoZhechev <mishomishev024@gmail.com>
2024-06-04 09:52:40 +0300

    Amount validation implemented

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ public class Program
 {
     private static string apiKey = null!;
 
+    private const string AMOUNT_MESSAGE = "Enter a possitive number with not more than 2 digits after the decimal separator:";
+
     public static void Main(string[] args)
     {
         LoadApiKey();
@@ -20,6 +22,12 @@ public class Program
         {
             Console.WriteLine("Please enter conversion rate date in the format YYYY-MM-DD:");
             dateInput = Console.ReadLine()!;
+        }
+
+        while (true)
+        {
+            var amount = GetValidAmout(AMOUNT_MESSAGE);
+            if (amount == -1) break;
         }
     }
     private static void LoadApiKey()
@@ -40,6 +48,34 @@ public class Program
     private static bool ValidateDateIput(string dateInput)
     {
         return DateTime.TryParseExact(dateInput, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out _);
+    }
+
+    private static decimal GetValidAmout(string promptMessage)
+    {
+        while (true)
+        {
+            Console.WriteLine(promptMessage);
+
+            var amountInput = Console.ReadLine()!;
+            if (amountInput.ToUpper() == "END") return -1;
+
+            if (decimal.TryParse(amountInput, out decimal amount) && amount > 0
+                && TwoDigitsAfterSeparator(amountInput)) return amount;
+
+            Console.WriteLine("Invalid amount!");
+        }
+
+    }
+    private static bool TwoDigitsAfterSeparator(string amountInput)
+    {
+        var parts = amountInput.Split(".");
+
+        if (parts.Length == 1) return true;
+
+        if (parts[1].Length > 2) return false;
+
+        return true;
+
     }
 }
 
